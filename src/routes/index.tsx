e811d6eca002515bf07766695c82f4b9b30856f3
tsots <mosalatsotetsi@gmail.com>
2025-10-15 01:26:29 +0200

    redirect page instruction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
+  beforeLoad: () => {
+    throw redirect({ to: "/products" });
+  },
   component: () => <>
     <p>About our noodle store 🍜</p>
     <h1>Are we there yet</h1>
